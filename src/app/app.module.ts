f183e694e9a0c3dc56cac4743db325ed822e2ffe
jay <9822jaykatwate@gmail.com>
2023-09-06 20:02:01 +0530

    Home nav bar WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
 import { SharedModule } from "./shared/shared.module";
+import { NavBarComponent } from './components/home/nav-bar/nav-bar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        AboutComponent,
+        NavBarComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
